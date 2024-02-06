84f43a2a6cf9434cbcd29cbbacc92ea272a93c7a
Giuseppe Caruso <giuseppe.caruso1@vodafone.com>
2024-02-06 14:20:57 +0100

    feature(component): add red button

---
+++ src/lib/Button.svelte
@@ -0,0 +1,24 @@
+<script lang="ts">
+  export let handleClick: () => void;
+</script>
+
+<button on:click={handleClick}>
+  <slot />
+</button>
+
+<style>
+  button {
+    background-color: red;
+    color: white;
+    padding: 10px 20px;
+    border: none;
+    border-radius: 5px;
+    cursor: pointer;
+  }
+  button:hover {
+    background-color: rgb(207, 2, 2);
+  }
+  button:active {
+    background-color: rgb(255, 93, 93);
+  }
+</style>
